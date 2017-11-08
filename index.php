d1ad93002a9f7b113f10704c9fa05af3c86d8581
Sonterix <sonterixo@gmail.com>
2017-11-08 23:30:25 +0200

    added project

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Root</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+    <div class="wrapper-grid">
+            <header id="header">
+                <nav id="nav">
+                    <img src="img/logo.png" alt="logo">
+                    <ul>
+                        <a href=""><li>home</li></a>
+                        <a href=""><li>gallery</li></a>
+                        <a href=""><li>about us</li></a>
+                        <a href=""><li>contuct</li></a>
+                    </ul>
+                </nav>
+                <div id="header-content">
+                    <h2>best selling products</h2>
+                    <p>lorem ipsum dolor sit, amet consectetur adipisicing elit</p>
+                </div>
+            </header>
+            
+            <section id="content">
+                <div class="content-inner" id="content-1">
+
+                </div>
+                <div class="content-inner" id="content-2">
+
+                </div>
+                <div class="content-inner" id="content-3">
+
+                </div>
+                <div class="content-inner" id="content-4">
+
+                </div>
+                <div class="content-inner" id="content-5">
+
+                </div>
+                <div class="content-inner" id="content-6">
+
+                </div>
+                <div class="content-inner" id="content-7">
+
+                </div>
+                <div class="content-inner" id="content-8">
+
+                </div>
+                <div class="content-inner" id="content-9">
+
+                </div>
+                <div class="content-inner" id="content-10">
+
+                </div>
+                <div class="content-inner" id="content-11">
+
+                </div>
+                <div class="content-inner" id="content-12">
+
+                </div>
+            </section>
+        
+            <footer id="footer">
+                <p>©Nick Burovskiy</p>
+                <p>2017-2018</p>
+            </footer>
+    </div>
+    
+    <!-- JS -->
+    <script src="js/main.js"></script>
+</body>
+</html>
